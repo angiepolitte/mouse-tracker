3e4934b33c815392282a4438e70f50e033461f96
Angie <angiepolitte@gmail.com>
2025-05-26 11:08:39 -0500

    updating custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import ParentComponent from "./ParentComponent";
 import useCounter from "./useCounter";
 
 function App() {
-  const { count, increment, decrement } = useCounter();
+  const { count, increment, decrement, reset } = useCounter();
   return (
     <div>
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
       <ParentComponent />
     </div>
   );
